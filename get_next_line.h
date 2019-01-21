5ea8e237f15c5aa3825729f91ac6ca19e57e8b54
loc-ed <41115064+loc-ed@users.noreply.github.com>
2019-01-21 12:12:30 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfrederi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/24 15:35:16 by cfrederi          #+#    #+#             */
+/*   Updated: 2018/06/25 17:05:16 by cfrederi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+
+# define BUFF_SIZE 8
+
+int	get_next_line(const int fd, char **line);
+
+#endif
